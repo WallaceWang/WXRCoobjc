fd36e8ae2bc5898fdcce8f93692179a7255cde03
wangxiaorui19 <wangxiaorui19@jd.com>
2021-09-06 09:41:07 +0800

    code

--- WXRCoobjc/core/csp.m
+++ WXRCoobjc/core/csp.m
@@ -24,22 +24,22 @@ static int _altcanexec(chan_alt_t *a) {
     alt_queue_t *altqueue;
     co_channel_t *c = a->channel;
     if (c->buffer.size == 0) {
-        altqueue = _chanarray(c, otherop(a->op));
+        altqueue = _chanarray(c, otherop(a->op)); // 取反操作，如果是send返回recqueue,如果是recive返回sendqueue.
         return altqueue && altqueue->count;
-    } else if (c->buffer.expandsize) {
+    } else if (c->buffer.expandsize) { // 如果可以动态扩展
         switch (a->op) {
-            case CHANNEL_SEND:
+            case CHANNEL_SEND: // send 永远可以
                 return 1;
-            case CHANNEL_RECEIVE:
+            case CHANNEL_RECEIVE: // recive需要有缓存的元素
                 return c->buffer.count > 0;
             default:
                 return 0;
         }
-    } else {
+    } else { // 不可以动态扩展容量，但是有个固定的容量
         switch (a->op) {
-            case CHANNEL_SEND:
+            case CHANNEL_SEND: // 如果是send，要看是否容量满了
                 return c->buffer.count < c->buffer.size;
-            case CHANNEL_RECEIVE:
+            case CHANNEL_RECEIVE: // 如果是recive, 要看是否有元素存在
                 return c->buffer.count > 0;
             default:
                 return 0;
@@ -49,12 +49,20 @@ static int _altcanexec(chan_alt_t *a) {
     return 0;
 }
 
+static int _altexec(chan_alt_t *a){
+    alt_queue_t *altqueue;
+    chan_alt_t
+    return 0;
+}
+
 static int _chanalt(chan_alt_t *a) {
     coroutine_t *t = coroutine_self();
     a->task = t;
     co_channel_t *c = a->channel;
 //    chanlock(c);
-    
+    if (_altcanexec(a)) {
+        return _altexec(a);
+    }
     return 0;
 }
 
